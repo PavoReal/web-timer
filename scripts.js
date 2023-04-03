e45b57d66afcb8a2b6497a2148ade9765392aa50
Garrison Peacock <garrison.peacock@gmail.com>
2023-04-03 12:37:40 -0700

    updated beep

--- scripts.js
+++ scripts.js
@@ -1,42 +1,54 @@
 const timerInput = document.getElementById('timerInput');
 const startBtn = document.getElementById('startBtn');
 const timerDisplay = document.getElementById('timerDisplay');
+const resetBtn = document.createElement('button');
 let timer;
 
 function formatTime(milliseconds) {
     const totalSeconds = Math.floor(milliseconds / 1000);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
-    const ms = milliseconds % 1000;
     
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${ms.toString().padStart(3, '0')}`;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
 function updateTimerDisplay(time) {
     timerDisplay.textContent = formatTime(time);
 }
 
-startBtn.addEventListener('click', () => {
-    let time = parseInt(timerInput.value, 10) * 1000;
+function flashBackgroundColor(duration) {
+    document.body.style.backgroundColor = "green";
+    setTimeout(() => {
+        document.body.style.backgroundColor = "";
+    }, duration);
+}
 
-    if (isNaN(time) || time < 1) {
-        alert('Please enter a valid time in seconds.');
-        return;
-    }
+startBtn.addEventListener('click', () => {
+    let time = 10 * 60 * 1000; // 10 minutes in milliseconds
 
     clearInterval(timer);
     updateTimerDisplay(time);
-    
+
     timer = setInterval(() => {
         if (time <= 0) {
             clearInterval(timer);
             updateTimerDisplay(0);
-            if (window.navigator && window.navigator.vibrate) {
-                window.navigator.vibrate(1000);
-            }
+            flashBackgroundColor(5000);
         } else {
-            time -= 10;
+            time -= 1000;
             updateTimerDisplay(time);
+            if (time % (60 * 1000) === 0) {
+                flashBackgroundColor(1000);
+            }
         }
-    }, 10);
+    }, 1000);
 });
+
+resetBtn.textContent = 'Reset';
+resetBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    document.body.style.backgroundColor = "";
+    updateTimerDisplay(0);
+});
+
+document.querySelector('.container').appendChild(resetBtn);
