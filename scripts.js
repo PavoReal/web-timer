176df6f8318a935fa413412a7a9d5a0cd324137a
Garrison Peacock <garrison.peacock@gmail.com>
2023-04-03 14:25:19 -0700

    Pause

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 const startBtn = document.getElementById('startBtn');
+const pauseBtn = document.getElementById('pauseBtn');
 const timerDisplay = document.getElementById('timerDisplay');
 const resetBtn = document.getElementById('resetBtn');
 let timer;
+let isPaused = false;
 
 function formatTime(milliseconds) {
     const totalSeconds = Math.floor(milliseconds / 1000);
@@ -24,27 +26,37 @@ function flashBackgroundColor(duration) {
 
 startBtn.addEventListener('click', () => {
     let time = 10 * 60 * 1000; // 10 minutes in milliseconds
+    isPaused = false;
 
     clearInterval(timer);
     updateTimerDisplay(time);
 
     timer = setInterval(() => {
-        if (time <= 0) {
-            clearInterval(timer);
-            updateTimerDisplay(0);
-            flashBackgroundColor(5000);
-        } else {
-            time -= 1000;
-            updateTimerDisplay(time);
-            if (time % (60 * 1000) === 0) {
-                flashBackgroundColor(2000);
+        if (!isPaused) {
+            if (time <= 0) {
+                clearInterval(timer);
+                updateTimerDisplay(0);
+                flashBackgroundColor(5000);
+            } else {
+                time -= 1000;
+                updateTimerDisplay(time);
+                if (time % (60 * 1000) === 0) {
+                    flashBackgroundColor(1000);
+                }
             }
         }
     }, 1000);
+});
+
+pauseBtn.addEventListener('click', () => {
+    isPaused = !isPaused;
+    pauseBtn.textContent = isPaused ? 'Unpause' : 'Pause';
 });
 
 resetBtn.addEventListener('click', () => {
     clearInterval(timer);
     document.body.style.backgroundColor = "";
     updateTimerDisplay(0);
+    isPaused = false;
+    pauseBtn.textContent = 'Pause';
 });
